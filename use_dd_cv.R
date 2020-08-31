63004876042bcf20449a392e0565afbcab7090e4
Megan Beckett <megan.beckett13@gmail.com>
2020-08-31 15:21:08 +0200

    Change html location

--- use_dd_cv.R
+++ use_dd_cv.R
@@ -4,7 +4,7 @@ datadrivencv::use_datadriven_cv(
   full_name = "Megan Beckett",
   data_location = "https://docs.google.com/spreadsheets/d/1QZCS5H0HE0LwcgEQkf6ZF4irRdSAjKLLnRql8EdSfew/edit?usp=sharing",
   pdf_location = "https://github.com/MeganBeckett/cv/raw/master/mbeckett_cv.pdf",
-  html_location = "https://github.com/MeganBeckett/cv/master/mbeckett_cv.html",
+  html_location = "https://meganbeckett.github.io/mbeckett_cv",
   source_location = "https://github.com/MeganBeckett/cv",
   output_dir = here()
 )
