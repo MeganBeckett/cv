3a07ec7b5e3d6d0fc7b5d592ac2f130ba82aca71
Megan Beckett <megan.beckett13@gmail.com>
2020-08-31 11:27:11 +0200

    Render CV; include image

---
+++ use_dd_cv.R
@@ -0,0 +1,10 @@
+library(datadrivencv)
+library(here)
+datadrivencv::use_datadriven_cv(
+  full_name = "Megan Beckett",
+  data_location = "https://docs.google.com/spreadsheets/d/1QZCS5H0HE0LwcgEQkf6ZF4irRdSAjKLLnRql8EdSfew/edit?usp=sharing",
+  pdf_location = "https://github.com/MeganBeckett/cv/raw/master/mbeckett_cv.pdf",
+  html_location = "https://github.com/MeganBeckett/cv/master/mbeckett_cv.html",
+  source_location = "https://github.com/MeganBeckett/cv",
+  output_dir = here()
+)
