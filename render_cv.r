45481514ab05ddf470516e0a406cea6e6ccb6a09
Megan Beckett <megan.beckett13@gmail.com>
2020-08-31 15:27:13 +0200

    Knit for github pages too

--- render_cv.r
+++ render_cv.r
@@ -6,9 +6,15 @@
 # for the HTML and PDF rendering. This exercise is left to the reader.
 
 # Knit the HTML version
+# During development and testing
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "mbeckett_cv.html")
+
+# For github pages - push in repo when happy
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "~/megan/MeganBeckett.github.io/mbeckett_cv.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
